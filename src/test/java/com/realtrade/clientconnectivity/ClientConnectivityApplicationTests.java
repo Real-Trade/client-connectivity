fb6148ddd21385db841d1dd079c5be1abce3ce77
enaannan <enaannan@gmail.com>
2021-03-12 23:02:31 +0000

    initial commit

---
+++ src/test/java/com/realtrade/clientconnectivity/ClientConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtrade.clientconnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
