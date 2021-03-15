83e424906a18a056d22fe7946996ebc7eccfcc08
enaannan <enaannan@gmail.com>
2021-03-15 21:22:05 +0000

    as

---
+++ client-connectivity/src/main/java/com/realtrade/clientconnectivity/ClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.realtrade.clientconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConnectivityApplication.class, args);
+	}
+
+}
