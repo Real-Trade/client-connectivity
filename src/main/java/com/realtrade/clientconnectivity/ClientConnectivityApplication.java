662dab055ca41b82b9a19e31367c126885def027
enaannan <enaannan@gmail.com>
2021-03-15 00:03:57 +0000

    added an endpoint for home

--- src/main/java/com/realtrade/clientconnectivity/ClientConnectivityApplication.java
+++ src/main/java/com/realtrade/clientconnectivity/ClientConnectivityApplication.java
@@ -2,11 +2,19 @@ package com.realtrade.clientconnectivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Controller
 @SpringBootApplication
 public class ClientConnectivityApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home (){
+		return "Hello world";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientConnectivityApplication.class, args);
 	}
