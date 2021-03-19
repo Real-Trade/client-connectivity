ec9ff66c8ef971453956528b2c4b46df843b6af9
enaannan <enaannan@gmail.com>
2021-03-19 14:24:40 +0000

    implemented admin login and registration validation for client

--- src/main/java/com/realtrade/clientconnectivity/services/ClientDatabaseService.java
+++ src/main/java/com/realtrade/clientconnectivity/services/ClientDatabaseService.java
@@ -2,7 +2,6 @@ package com.realtrade.clientconnectivity.services;
 
 import com.realtrade.clientconnectivity.dao.ClientDao;
 import com.realtrade.clientconnectivity.dto.Client;
-import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,24 +13,50 @@ public class ClientDatabaseService {
 // makes a call to the client repo passing in the email and returning a matching client
     //TODO: clientDao has an exists method, use that instead
 public String login(String email){
-    Client client = new Client();
-    client =clientDao.findByEmail(email);
-    if (client!= null){
-        System.out.println(client);
-        return "login successful";
+    if (emailIsValid(email)){
+        Client client = new Client();
+        client =clientDao.findByEmail(email);
+        if (client!= null){
+            System.out.println(client);
+            return "login successful";
+        }
+        return "this email is not in our database. Register?";
     }
-    return "login unsuccessful";
+    else return "error with email field";
 }
 
-//TODO: handle case where registration is not successful
+// logic to validate email field passed from controller
+    private boolean emailIsValid(String email) {
+
+    if(email != null ){
+        return true;
+    }else{
+        return false;
+    }
+
+    }
+
+    //TODO: handle case where registration is not successful
 public String register (Client client){
-client.setStatus(0);
-client.setAccountNumber(generateAccountNumber());
-    clientDao.save(client);
-    return "successful registration";
+
+    if (clientIsValid(client)){
+        client.setStatus(0);
+        client.setAccountNumber(generateAccountNumber());
+        clientDao.save(client);
+        return "successful registration";
+    }
+    return "error with fields passed";
 }
 
-//logic to generate account number
+//validate registration fields
+    private boolean clientIsValid(Client client) {
+    if (client.getEmail() != null && client.getFirstname() != null && client.getLastname()!= null && client.getPassword()!= null){
+        return true;
+    }
+    return false;
+    }
+
+    //logic to generate account number
     int account =10000;
 private int generateAccountNumber(){
   return   account++;
